13196562d5da9d1bfc8fe664a65e8a8c2c148876
edmo-web-khizar <khizaruddin.syed@goedmo.com>
2026-01-02 18:23:13 +0530

    transaction id mismatch added

--- src/modules/payment/services/payment.service.ts
+++ src/modules/payment/services/payment.service.ts
@@ -56,6 +56,12 @@ export class PaymentService {
       dto.subscriptionId,
     );
 
+    const userSubscription =
+      await this.paymentRepository.findLatestSubscriptionByUserId(userId);
+
+    if (userSubscription?.transactionId !== subscription?.transactionId) {
+      throw new BadRequestException('Transaction ID mismatched');
+    }
     if (!subscription) {
       throw new NotFoundException('Subscription not found');
     }
