7afc9bd1951bec962ec2d8293fda15f18781ca74
edmo-web-khizar <khizaruddin.syed@goedmo.com>
2026-02-10 23:19:04 +0530

    changes changed default port to 8080

--- src/main.ts
+++ src/main.ts
@@ -84,7 +84,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
 
   await app.listen(port, '0.0.0.0');
 
